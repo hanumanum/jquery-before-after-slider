3fe182b6f51cbf35b2e89d44dbf94cfd9bddc747
artak (hanuman) kolyan <a.kolyan@gratun.am>
2017-10-23 15:59:03 +0400

    first version that works :Ճ

--- before-after-slider.js
+++ before-after-slider.js
@@ -1,48 +1,40 @@
-(function( $ ) {
-    
-       $.fn.hanBeforeAfterSlider = function(c) {
-            //console.log(c);
-            var point = parseInt(c.width)/2;
-            $(this).css("width",c.width);
-            $(this).css("height",c.height);
-            $(this).css("margin","0 auto");
-            $(this).css("position","relative");
+(function($) {
 
+    $.fn.hanBeforeAfterSlider = function(c) {
+        //console.log(c);
+        var point = parseInt(c.width) / 2;
+        $(this).css({
+            "width": c.width,
+            "height": c.height
+        });
 
-            var i1 = document.createElement("img");
-            var i2 = document.createElement("div");
+        var i1 = document.createElement("img");
+        var i2 = document.createElement("div");
 
-            $(i1).attr("id","firstImage");
-            $(i1).attr("src",c.firstImage);
-            $(i1).css({
-                "width":c.width,
-                "height":"auto",
-                "position":"absolute",
-            });
+        $(i1).attr("src", c.firstImage);
+        $(i1).css({
+            "height": c.height,
+            "position": "absolute",
+        });
 
-            
+        $(i2).css({
+            "background-image": "url(" + c.secondImage + ")",
+            "background-size": "cover",
+            "width": c.start,
+            "height": c.height,
+            "position": "absolute",
+            "background-repeat": "no-repeat",
+            "border-right": "1px solid gray"
+        });
 
-            $(i2).css({
-                "background-image":"url("+c.secondImage+")",
-                "background-size":"cover",
-                "width":point+"px",
-                "position":"absolute",
-                "background-repeat": "no-repeat",
-                "border-right":"1px solid gray"
-            });
+        var offset = $(i2).offset();
+        $(this).mousemove(function(event) {
+            offset = $(i2).offset();
+            $(i2).css("width", event.pageX - offset.left);
+        });
 
-            
-            $(this).append(i1);
-            var h = document.getElementById('firstImage').getBoundingClientRect();;
-            console.log(h);
+        $(this).append(i1);
+        $(this).append(i2);
+    };
 
-            //var height = $(i1).height();
-            console.log("height",h);
-            //$(i2).css("height",h+"px");
-            $(this).append(i2);
-
-            console.log("point",point);
-            
-        };
-    
-   }( jQuery ));
+}(jQuery));
