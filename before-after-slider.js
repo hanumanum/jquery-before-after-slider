2880259c1e78617044459eed1823055300a3ece4
a. kolyan aka hanuman <a.kolyan@gratun.am>
2017-10-23 11:22:21 +0000

    simple experiments

--- before-after-slider.js
+++ before-after-slider.js
@@ -6,33 +6,42 @@
             $(this).css("width",c.width);
             $(this).css("height",c.height);
             $(this).css("margin","0 auto");
+            $(this).css("position","relative");
 
-            var i1 = document.createElement("div");
+
+            var i1 = document.createElement("img");
             var i2 = document.createElement("div");
 
+            $(i1).attr("id","firstImage");
+            $(i1).attr("src",c.firstImage);
             $(i1).css({
-                "background-image":"url("+c.firstImage+")",
-                "background-size":"cover",
                 "width":c.width,
-                "height":c.height,
+                "height":"auto",
                 "position":"absolute",
-                "background-repeat": "no-repeat",
             });
+
+            
 
             $(i2).css({
                 "background-image":"url("+c.secondImage+")",
                 "background-size":"cover",
                 "width":point+"px",
-                "height":c.height,
                 "position":"absolute",
                 "background-repeat": "no-repeat",
                 "border-right":"1px solid gray"
             });
 
+            
             $(this).append(i1);
+            var h = document.getElementById('firstImage').getBoundingClientRect();;
+            console.log(h);
+
+            //var height = $(i1).height();
+            console.log("height",h);
+            //$(i2).css("height",h+"px");
             $(this).append(i2);
 
-            console.log(point);
+            console.log("point",point);
             
         };
     
